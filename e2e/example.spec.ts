d58c5c7ef20bbbbdb6eb210ec963d47ce83aacd8
Dmitry Munda <dmitrijs.munda@onairentertainment.com>
2021-10-12 11:26:13 +0300

    spec update

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,7 +1,12 @@
 // example.spec.ts
 import { test, expect } from '@playwright/test';
 
-test('example test', async ({ page }) => {
+test('example test fails', async ({ page }) => {
   await page.goto('https://playwright.dev');
   expect(await page.screenshot()).toMatchSnapshot('landing.png', {threshold:0.7});
-});
+})
+
+test('example test passes', async ({ page }) => {
+  await page.goto('https://playwright.dev');
+  expect(await page.screenshot()).toMatchSnapshot('landing.png', {threshold:0.8});
+})
